a77ef382a6ab8cb316922331c4b67c13622f4df9
Charlie Friend <charles.d.friend@gmail.com>
2019-04-17 20:23:27 -0700

    Add function for converting sqlalchemy rows to dictionaries

--- db/db_utils.py
+++ db/db_utils.py
@@ -1,3 +1,4 @@
+import re
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db.db_models import Base
@@ -16,3 +17,36 @@ def get_db_session(db_name):
     DBSession.bind = engine
 
     return DBSession()
+
+
+def model2dict(model, session):
+    d = {}
+    for column in model.__table__.columns:
+        if len(column.foreign_keys) == 1:
+            # Recursively traverse foreign keys
+            id = getattr(model, column.name)
+            foreign_key = next(key for key in column.foreign_keys)
+
+            query = session.query(foreign_key.column.table).filter(foreign_key.column == id)
+
+            col_name = re.sub("_Id$", "", column.name)
+            if query.count() == 1:
+                result = query.one()
+                d[col_name] = _result_to_dict(query, result)
+            else:
+                d[col_name] = None
+
+        elif len(column.foreign_keys) > 1:
+            raise Exception("Can't handle more than one foreign key reference in a column!")
+        else:
+            d[column.name] = getattr(model, column.name)
+
+    return d
+
+
+def _result_to_dict(query, row):
+    d = {}
+    col_descs = query.column_descriptions
+    for desc, item in zip(col_descs, row):
+        d[desc["name"]] = item
+    return d
